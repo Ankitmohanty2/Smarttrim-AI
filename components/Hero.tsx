3e143c46bd6908c2032bece9a6d97bc23f88efcf
Ankitmohanty2 <mohantyankit61@gmail.com>
2025-01-22 13:04:57 +0530

    Initial commit: AI Background Remover project

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ export function Hero() {
             )}
         >
             <AnimatedShinyText className="inline-flex items-center   justify-center px-4 py-1 transition ease-out  hover:duration-300 hover:text-neutral-400">
-            <span>✨Magic Background Remover</span>
+            <span>✨Magic Background Remove</span>
             <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
             </AnimatedShinyText>
         </div>
